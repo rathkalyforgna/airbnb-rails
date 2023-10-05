ed490ecc78dd3fe1fa8d4be35038222dfd47d72d
rathkalyforgna <rathkalyforgna@gmail.com>
2023-10-05 22:32:43 +1100

    feat: add close function of auth modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,19 +2,24 @@ import { Controller } from "@hotwired/stimulus";
 import { enter, leave } from "el-transition";
 
 export default class extends Controller {
+  static targets = ["closeButton"];
   connect() {
-    document.getElementById("modal").addEventListener("click", this.closeModal);
+    document.getElementById("modal").addEventListener("click", (event) => {
+      const modalPanelClicked = document
+        .getElementById("modal-panel")
+        .contains(event.target);
+      if (!modalPanelClicked && event.target.id !== "modal-trigger") {
+        this.closeModal();
+      }
+    });
+
+    this.closeButtonTarget.addEventListener("click", this.closeModal);
   }
 
   closeModal(event) {
-    const modalPanelClicked = document
-      .getElementById("modal-panel")
-      .contains(event.target);
-    if (!modalPanelClicked && event.target.id !== "modal-trigger") {
-      leave(document.getElementById("modal"));
-      leave(document.getElementById("modal-backdrop"));
-      leave(document.getElementById("modal-panel"));
-    }
+    leave(document.getElementById("modal"));
+    leave(document.getElementById("modal-backdrop"));
+    leave(document.getElementById("modal-panel"));
   }
 
   openModal() {
